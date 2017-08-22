501eea8716ccb4259539f7971448b5de9de12b33
Percy Salgado <percysalgado@icloud.com>
2017-08-21 19:36:24 -0500

    Utilidad para eliminar todo rastro de Adobe CC en macOS

---
+++ Desinstalar todo Adobe CC.sh
@@ -0,0 +1,15 @@
+#########################################################
+#
+# UTILIDAD PARA DESINSTALAR ADOBE
+#
+rm -dfr /Applications/Adobe*
+rm -dfr /Applications/Utilities/Adobe*
+rm -dfr /Library/Application\ Support/Adobe
+rm -dfr /Library/Application\ Support/regid*
+rm -dfr /var/root/Library/Application\ Support/Adobe
+rm -dfr /Users/*/Library/Application\ Support/Adobe
+echo 
+echo
+echo "******************* SUITE ADOBE CC DESINSTALADO *******************"
+echo
+exit 0
