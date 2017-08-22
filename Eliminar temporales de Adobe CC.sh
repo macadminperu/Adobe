4e8c9739ebe522b52451b73cc236594bcd6fd7b8
Percy Salgado <percysalgado@icloud.com>
2017-08-21 19:49:55 -0500

    Elimina temporales de Adobe CC

---
+++ Eliminar temporales de Adobe CC.sh
@@ -0,0 +1,10 @@
+#########################################################
+#
+# UTILIDAD PARA ELIMINAR TEMPORALES DE ADOBE - CUANDO LA APLICACION SE VUELVE INESTABLE
+#
+rm -dfrv ~/Library/Application\ Support/Adobe
+echo 
+echo
+echo "******************* TEMPORALES ELIMINADOS *******************"
+echo
+exit 0
