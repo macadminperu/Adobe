b6e03ccc170baaa82b3f01fbb3a3c44604a8df60
Carlos Salgado <percysalgado@icloud.com>
2021-11-08 19:40:35 -0500

    Update Desinstalar todo Adobe CC.sh

--- Desinstalar todo Adobe CC.sh
+++ Desinstalar todo Adobe CC.sh
@@ -1,15 +1,27 @@
-#########################################################
-#
-# UTILIDAD PARA DESINSTALAR ADOBE
-#
-rm -dfr /Applications/Adobe*
-rm -dfr /Applications/Utilities/Adobe*
-rm -dfr /Library/Application\ Support/Adobe
-rm -dfr /Library/Application\ Support/regid*
-rm -dfr /var/root/Library/Application\ Support/Adobe
-rm -dfr /Users/*/Library/Application\ Support/Adobe
+clear
+echo "****************** UTILIDAD PARA DESINSTALAR ADOBE ****************"
+echo
+echo
+echo ESTAS SEGURO DE DESINSTALAR TODO ADOBE?
+echo
+echo "SI TIENE PRESETS, PROCEDA A BACKUPEAR ANTES DE CONTINUAR ..!"
 echo 
+echo "ENTER PARA CONTINUAR, CTL-C PARA CANCELAR ..."
+echo
+read
+echo "Ingrese el password de su Mac:"
+echo
+sudo echo
+sudo killall ACCFinderSync “Core Sync” AdobeCRDaemon “Adobe Creative” AdobeIPCBroker node “Adobe Desktop Service” “Adobe Crash Reporter”
+sudo kill -9 $(ps ax | grep Adobe | awk '{print $1}') &>/dev/null
+sudo rm -dfrv /Applications/Adobe*
+sudo rm -dfrv /Applications/Utilities/Adobe*
+sudo rm -dfrv “/Library/Caches/.”* “/private/tmp/zx”* 
+sudo rm -dfrv /Library/Application\ Support/Adobe
+sudo rm -dfrv /Library/Application\ Support/regid*
+sudo rm -dfrv /var/root/Library/Application\ Support/Adobe
+sudo rm -dfrv /Users/*/Library/Application\ Support/Adobe /Users/*/Library/Preferences/Adobe
 echo
 echo "******************* SUITE ADOBE CC DESINSTALADO *******************"
-echo
-exit 0
+
+
